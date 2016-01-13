a2983958cbe804cb5995b757154207167954ee95
wangshuo95 <786690633@qq.com>
2016-01-13 14:09:57 +0800

    更新

--- 100/w/js/main.js
+++ 100/w/js/main.js
@@ -1,42 +1,4 @@
-var i=0; //记录第几张图片
-var inter;//存放计时器
-//切换图片
-function pic(){
-  if(i==4){
-	  i=0;
-	  $(".tupian").animate({left:"0"},800,function(){$(".num li:first").addClass("cur").siblings().removeClass("cur");})
-	  }
-	else{
-	   var w=$(".banner").width();
-		$(".tupian").animate({left:"-="+w},800,function(){
-			$(".num li:eq("+i+")").addClass("cur").siblings().removeClass("cur");
-		})
-	i++;
-		}
-	}
-	//当悬停在li上的时候切换图片
-	function cs(){
-		var k=i*w;
-		$(".tupian").stop(true,true).animate({left:"-"+k},800)
-		
-		}
-	//加载函数
-$(function(){
-	inter=setInterval(pic,3000);
-	$(".num li").hover(function(){
-		//停止计时器
-		clearInterval(inter);
-		var a=$(this).text();
-		//转换类型
-		i=parseInt(a)-1;
-	cs();
-	$(this).addClass("cur").siblings().removeClass("cur");
-		},function(){
-			
-		inter=setInterval(pic,3000);
-			
-	})
-	})
+
 $(function(){
 		$(".one").kxbdMarquee({direction:"left"});
 		
